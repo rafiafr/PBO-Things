72edffd068b12bfcdd3985e828f42adbed0c1ec5
RAFI AL FIRDAUS RAMAHDAN <rafialfirdaus9@gmail.com>
2026-05-13 10:11:19 +0700

    Implement library management system in Python

---
+++ utees/got-u-two.py
@@ -0,0 +1,211 @@
+import random
+import datetime
+
+class SistemPerpustakaan:
+    def __init__(self):
+        # Inisialisasi database penyimpanan
+        self.data_buku = {}
+        self.data_anggota = {}
+        self.riwayat_transaksi = []
+        self.transaksi_counter = 1
+
+    def get_tanggal_sekarang(self):
+        return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    def tambah_buku(self):
+        print("\n--- Tambah Data Buku ---")
+        while True:
+            kode_buku = input("Masukkan Kode Buku: ")
+            if kode_buku in self.data_buku:
+                print("Notif: Kode sama! Kode buku sudah ada, silakan masukkan kode lain.")
+            else:
+                break
+        
+        judul_buku = input("Judul Buku: ")
+        penulis = input("Penulis: ")
+        tahun_terbit = input("Tahun Terbit: ")
+        
+        self.data_buku[kode_buku] = {
+            'judul': judul_buku,
+            'penulis': penulis,
+            'tahun': tahun_terbit,
+            'status': 'Tersedia'
+        }
+        print("Data buku berhasil ditambahkan!")
+
+    def tambah_anggota(self):
+        print("\n--- Tambah Data Anggota ---")
+        nama = input("Nama: ")
+        alamat = input("Alamat: ")
+        no_hp = input("No HP: ")
+        
+        no_anggota = "ANG" + str(random.randint(1000, 9999))
+        self.data_anggota[no_anggota] = {
+            'nama': nama,
+            'alamat': alamat,
+            'no_hp': no_hp
+        }
+        print(f"Data anggota berhasil disimpan. Nomor anggota Anda: {no_anggota}")
+
+    def pinjam_buku(self):
+        print("\n--- Pinjam Buku ---")
+        no_anggota = input("Masukkan Nomor Anggota: ")
+        
+        if no_anggota not in self.data_anggota:
+            print("Nomor anggota tidak ditemukan!")
+            return
+            
+        anggota = self.data_anggota[no_anggota]
+        print(f"\nData Anggota -> Nama: {anggota['nama']}, Alamat: {anggota['alamat']}, No HP: {anggota['no_hp']}")
+        
+        while True:
+            kode_buku = input("Masukkan Kode Buku yang akan dipinjam: ")
+            if kode_buku not in self.data_buku:
+                print("Notif: Kode tidak tersedia.")
+            else:
+                buku = self.data_buku[kode_buku]
+                if buku['status'] != 'Tersedia':
+                    print(f"Buku '{buku['judul']}' tidak bisa dipinjam (Status: {buku['status']}).")
+                    break
+                else:
+                    no_transaksi = f"TRX{self.transaksi_counter:03d}"
+                    self.transaksi_counter += 1
+                    tgl_pinjam = self.get_tanggal_sekarang()
+                    
+                    buku['status'] = 'Dipinjam'
+                    
+                    transaksi = {
+                        'no_transaksi': no_transaksi,
+                        'no_anggota': no_anggota,
+                        'nama_anggota': anggota['nama'],
+                        'kode_buku': kode_buku,
+                        'judul_buku': buku['judul'],
+                        'tgl_pinjam': tgl_pinjam,
+                        'tgl_kembali': '-',
+                        'status': 'Dipinjam'
+                    }
+                    self.riwayat_transaksi.append(transaksi)
+                    
+                    print("\n-- Peminjaman Berhasil --")
+                    print(f"Nomor Transaksi : {no_transaksi}")
+                    print(f"Nama Anggota    : {anggota['nama']}")
+                    print(f"Judul Buku      : {buku['judul']}")
+                    print(f"Tanggal Pinjam  : {tgl_pinjam}")
+                    break
+
+    def kembalikan_buku(self):
+        print("\n--- Kembalikan Buku ---")
+        while True:
+            kode_buku = input("Masukkan Kode Buku yang dikembalikan: ")
+            if kode_buku not in self.data_buku:
+                print("Notif: Kode tidak tersedia.")
+            else:
+                buku = self.data_buku[kode_buku]
+                if buku['status'] == 'Tersedia':
+                    print("Notif: Buku ini tidak sedang dipinjam.")
+                    break
+                else:
+                    tgl_kembali = self.get_tanggal_sekarang()
+                    buku['status'] = 'Tersedia'
+                    
+                    for trx in reversed(self.riwayat_transaksi):
+                        if trx['kode_buku'] == kode_buku and trx['status'] == 'Dipinjam':
+                            trx['tgl_kembali'] = tgl_kembali
+                            trx['status'] = 'Dikembalikan'
+                            
+                            print("\n-- Pengembalian Berhasil --")
+                            print(f"Nomor Transaksi : {trx['no_transaksi']}")
+                            print(f"Nama Anggota    : {trx['nama_anggota']}")
+                            print(f"Judul Buku      : {trx['judul_buku']}")
+                            print(f"Tanggal Kembali : {tgl_kembali}")
+                            break
+                    break
+
+    def info_anggota(self):
+        print("\n--- Info Anggota ---")
+        while True:
+            no_anggota = input("Masukkan Nomor Anggota: ")
+            if no_anggota not in self.data_anggota:
+                print("Notif: Anggota tidak ditemukan.")
+                return
+            else:
+                break
+        
+        anggota = self.data_anggota[no_anggota]
+        buku_dipinjam = [trx for trx in self.riwayat_transaksi if trx['no_anggota'] == no_anggota and trx['status'] == 'Dipinjam']
+        
+        if len(buku_dipinjam) > 0:
+            print(f"\nHeader : Nomor Anggota: {no_anggota}, Nama: {anggota['nama']}, Alamat: {anggota['alamat']}, Nomor HP: {anggota['no_hp']}")
+            print("DAFTAR BUKU YANG SEDANG DIPINJAM:")
+            print(f"{'No':<5} | {'Kode Buku':<10} | {'Judul Buku':<25} | {'Status':<15}")
+            print("-" * 65)
+            for i, trx in enumerate(buku_dipinjam, 1):
+                print(f"{i:<5} | {trx['kode_buku']:<10} | {trx['judul_buku']:<25} | {trx['status']:<15}")
+        else:
+            print("Notif: Tidak ada buku yang dipinjam")
+
+    def daftar_buku(self):
+        print("\n--- Daftar Buku ---")
+        if not self.data_buku:
+            print("Notif: Belum ada data buku")
+        else:
+            print(f"{'No':<5} | {'Kode Buku':<10} | {'Judul Buku':<25} | {'Penulis':<20} | {'Tahun':<6} | {'Status':<15}")
+            print("-" * 90)
+            no = 1
+            for kode, info in self.data_buku.items():
+                print(f"{no:<5} | {kode:<10} | {info['judul']:<25} | {info['penulis']:<20} | {info['tahun']:<6} | {info['status']:<15}")
+                no += 1
+
+    def tampilkan_riwayat(self):
+        print("\nRiwayat peminjaman")
+        if not self.riwayat_transaksi:
+            print("Belum ada riwayat transaksi.")
+        else:
+            print(f"{'No':<4} | {'No Trx':<10} | {'Tgl Pinjam':<20} | {'Tgl Kembali':<20} | {'Nama Anggota':<15} | {'Kode Buku':<10} | {'Judul':<20} | {'Status'}")
+            print("-" * 125)
+            for i, trx in enumerate(self.riwayat_transaksi, 1):
+                print(f"{i:<4} | {trx['no_transaksi']:<10} | {trx['tgl_pinjam']:<20} | {trx['tgl_kembali']:<20} | {trx['nama_anggota']:<15} | {trx['kode_buku']:<10} | {trx['judul_buku']:<20} | {trx['status']}")
+
+    def jalankan(self):
+        while True:
+            print("\n" + "="*40)
+            print("SISTEM PERPUSTAKAAN")
+            print("="*40)
+            print("1. Tambah Data Buku")
+            print("2. Tambah Data Anggota")
+            print("3. Pinjam Buku")
+            print("4. Kembalikan Buku")
+            print("5. Info Anggota")
+            print("6. Daftar Buku")
+            print("7. Riwayat Peminjaman")
+            print("8. Keluar")
+            print("="*40)
+            
+            pilihan = input("Pilih menu (1-8): ")
+            
+            if pilihan == '1':
+                self.tambah_buku()
+            elif pilihan == '2':
+                self.tambah_anggota()
+            elif pilihan == '3':
+                self.pinjam_buku()
+            elif pilihan == '4':
+                self.kembalikan_buku()
+            elif pilihan == '5':
+                self.info_anggota()
+            elif pilihan == '6':
+                self.daftar_buku()
+            elif pilihan == '7':
+                self.tampilkan_riwayat()
+            elif pilihan == '8':
+                print("Keluar Program... Terima kasih!")
+                break
+            else:
+                print("Pilihan tidak valid. Silakan pilih menu 1-8.")
+
+# Cara mengeksekusi class tersebut:
+if __name__ == "__main__":
+    # Membuat objek dari class SistemPerpustakaan
+    aplikasi = SistemPerpustakaan()
+    # Menjalankan menu utama
+    aplikasi.jalankan()
